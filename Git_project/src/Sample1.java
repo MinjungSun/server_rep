088ae69de74044b76e54e4cbba32f71f93c20e26
MinjungSun <mjsun0123@gmail.com>
2019-04-17 15:51:18 +0900

    commit4

--- Git_project/src/Sample1.java
+++ Git_project/src/Sample1.java
@@ -3,5 +3,6 @@ public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("Hello, This is MJ");
 		System.out.println("Where Can I buy a cup of coffee?");
+		System.out.println("You should go to Coffee shop nearby.");
 	}//main
 }//end class
