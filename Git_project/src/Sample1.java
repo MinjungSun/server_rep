21b6eaae7d882dd6f7ca201d39c9070137576a1b
MinjungSun <mjsun0123@gmail.com>
2019-04-17 16:08:37 +0900

    commit6-Minjung

--- Git_project/src/Sample1.java
+++ Git_project/src/Sample1.java
@@ -5,5 +5,7 @@ public class Sample1 {
 		System.out.println("Where Can I buy a cup of coffee?");
 		System.out.println("You should go to Coffee shop nearby.");
 		System.out.println("But,,,I don't have enough money..... Will you buy for me?");
+		
+		System.out.println("Summer has come~ : Minjung...");
 	}//main
 }//end class
